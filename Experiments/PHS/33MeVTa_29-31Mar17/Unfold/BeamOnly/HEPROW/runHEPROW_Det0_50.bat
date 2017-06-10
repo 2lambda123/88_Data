341c297854f119a6b64dd58a01dfa0cb158a2fa0
James Bevins <james.e.bevins@gmail.com>
2017-06-09 23:53:55 -0700

    Updated clean beam unfold - Added Lab notebook pictures - Added Set-up Pictures - Updated 1 MeV unfold - Attempted to update 0.5 MeV unfold, but too many bins

--- Experiments/PHS/33MeVTa_29-31Mar17/Unfold/BeamOnly/HEPROW/runHEPROW_Det0_50.bat
+++ Experiments/PHS/33MeVTa_29-31Mar17/Unfold/BeamOnly/HEPROW/runHEPROW_Det0_50.bat
@@ -36,7 +36,7 @@ echo The GRAVELW run is now performed,
 echo ---------------------------------------------------------------------
 echo The input file is named grv.inp
 echo ***** There follow the screen output lines, they may here be ignored **
-gravelw Inputs\grv_50.inp
+gravelw Inputs\grv_Det0_50.inp
 echo ***********************************************************************
 echo +++++++ END of gravelw run, look at protocol at grv.log
 echo ***********************************************************************
@@ -46,7 +46,7 @@ echo *********************************************************************
 echo The UNFANAW run is now performed
 echo ****************** input file: UNF.inp
 echo ***********************************************************************
-unfanaw Inputs\unf_50.inp
+unfanaw Inputs\unf_Det0_50.inp
 echo ***********************************************************************
 echo +++++++ END of unfanaw run, look at protocol in: unf.log
 echo ***********************************************************************
@@ -57,7 +57,7 @@ echo The MIEKEW run is now performed, without additional covariance
 echo ****************** input file: MIK.inp
 echo ***********************************************************************
 echo program now starts with :
-MIEKEW Inputs\mik_50.inp
+MIEKEW Inputs\mik_Det0_50.inp
 echo ***********************************************************************
 echo +++++++ END of MIEKEW run, look at protocol in: mik.log
 echo ************************************************************************
@@ -68,15 +68,15 @@ echo The FALTW run is now performed, with additional covariance
 echo ****************** input file: faltw_50.inp
 echo ***********************************************************************
 echo program now starts with :
-faltw Inputs\faltw_50.inp
+faltw Inputs\faltw_Det0_50.inp
 echo ***********************************************************************
 echo +++++++ END of FALTW run, look at protocol in: faltw.log
 echo ************************************************************************
 pause
 cls
-move /y MIEKE.MTX MIEKE_50.MTX
-move /y MIEKE1.MTX MIEKE1_50.MTX
-move /y PROBFlux PROBFlux_50
+move /y MIEKE.MTX MIEKE_Det0_50.MTX
+move /y MIEKE1.MTX MIEKE1_Det0_50.MTX
+move /y PROBFlux PROBFlux_Det0_50
 del *.$$$
 del Inputs\*.$$$
 
