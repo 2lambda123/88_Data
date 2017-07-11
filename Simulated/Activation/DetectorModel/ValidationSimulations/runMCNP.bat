85ab92280cc464fbde4c3695a634682a506f3560
James Bevins <james.e.bevins@gmail.com>
2017-07-11 13:31:13 -0700

    Fixed BR error
    - Added BR to models
    - Moved base model to an expandable worm input
    - Reorganized file structure
    - Added analysis notebook

---
+++ Simulated/Activation/DetectorModel/ValidationSimulations/runMCNP.bat
@@ -0,0 +1,18 @@
+del HPGe_model_2_1_1.o 
+mcnp6 i=HPGe_model_2_1_1.in o=HPGe_model_2_1_1.o tasks 3
+del HPGe_model_2_1_18.o 
+mcnp6 i=HPGe_model_2_1_18.in o=HPGe_model_2_1_18.o tasks 3
+del HPGe_model_2_2_1.o 
+mcnp6 i=HPGe_model_2_2_1.in o=HPGe_model_2_2_1.o tasks 3
+del HPGe_model_2_2_18.o 
+mcnp6 i=HPGe_model_2_2_18.in o=HPGe_model_2_2_18.o tasks 3
+del HPGe_model_2_4_1.o 
+mcnp6 i=HPGe_model_2_4_1.in o=HPGe_model_2_4_1.o tasks 3
+del HPGe_model_2_4_18.o 
+mcnp6 i=HPGe_model_2_4_18.in o=HPGe_model_2_4_18.o tasks 3
+del HPGe_model_2_5_1.o 
+mcnp6 i=HPGe_model_2_5_1.in o=HPGe_model_2_5_1.o tasks 3
+del HPGe_model_2_5_18.o 
+mcnp6 i=HPGe_model_2_5_18.in o=HPGe_model_2_5_18.o tasks 3
+del *.$$$ 
+del runt* 
