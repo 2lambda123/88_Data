b5a8cf5bd7f3ba9c28a2eda40d4d680a05007e25
James Bevins <james.e.bevins@gmail.com>
2017-08-18 16:08:02 -0400

    Updated model with new calibration data.

--- Simulated/Activation/DetectorModel/ValidationSimulations/runMCNP.bat
+++ Simulated/Activation/DetectorModel/ValidationSimulations/runMCNP.bat
@@ -1,18 +1,92 @@
+del HPGe_model_1_1_1.o 
+mcnp6 i=HPGe_model_1_1_1.in o=HPGe_model_1_1_1.o tasks 2
+del runt* 
+del HPGe_model_1_1_18.o 
+mcnp6 i=HPGe_model_1_1_18.in o=HPGe_model_1_1_18.o tasks 2
+del runt* 
+del HPGe_model_1_1_5.o 
+mcnp6 i=HPGe_model_1_1_5.in o=HPGe_model_1_1_5.o tasks 2
+del runt* 
+del HPGe_model_1_2_1.o 
+mcnp6 i=HPGe_model_1_2_1.in o=HPGe_model_1_2_1.o tasks 2
+del runt* 
+del HPGe_model_1_2_18.o 
+mcnp6 i=HPGe_model_1_2_18.in o=HPGe_model_1_2_18.o tasks 2
+del runt* 
+del HPGe_model_1_2_5.o 
+mcnp6 i=HPGe_model_1_2_5.in o=HPGe_model_1_2_5.o tasks 2
+del runt* 
+del HPGe_model_1_3_1.o 
+mcnp6 i=HPGe_model_1_3_1.in o=HPGe_model_1_3_1.o tasks 2
+del runt* 
+del HPGe_model_1_3_18.o 
+mcnp6 i=HPGe_model_1_3_18.in o=HPGe_model_1_3_18.o tasks 2
+del runt* 
+del HPGe_model_1_3_5.o 
+mcnp6 i=HPGe_model_1_3_5.in o=HPGe_model_1_3_5.o tasks 2
+del runt* 
+del HPGe_model_1_4_1.o 
+mcnp6 i=HPGe_model_1_4_1.in o=HPGe_model_1_4_1.o tasks 2
+del runt* 
+del HPGe_model_1_4_18.o 
+mcnp6 i=HPGe_model_1_4_18.in o=HPGe_model_1_4_18.o tasks 2
+del runt* 
+del HPGe_model_1_4_5.o 
+mcnp6 i=HPGe_model_1_4_5.in o=HPGe_model_1_4_5.o tasks 2
+del runt* 
+del HPGe_model_1_5_1.o 
+mcnp6 i=HPGe_model_1_5_1.in o=HPGe_model_1_5_1.o tasks 2
+del runt* 
+del HPGe_model_1_5_18.o 
+mcnp6 i=HPGe_model_1_5_18.in o=HPGe_model_1_5_18.o tasks 2
+del runt* 
+del HPGe_model_1_5_5.o 
+mcnp6 i=HPGe_model_1_5_5.in o=HPGe_model_1_5_5.o tasks 2
+del runt* 
 del HPGe_model_2_1_1.o 
-mcnp6 i=HPGe_model_2_1_1.in o=HPGe_model_2_1_1.o tasks 3
+mcnp6 i=HPGe_model_2_1_1.in o=HPGe_model_2_1_1.o tasks 2
+del runt* 
 del HPGe_model_2_1_18.o 
-mcnp6 i=HPGe_model_2_1_18.in o=HPGe_model_2_1_18.o tasks 3
+mcnp6 i=HPGe_model_2_1_18.in o=HPGe_model_2_1_18.o tasks 2
+del runt* 
+del HPGe_model_2_1_5.o 
+mcnp6 i=HPGe_model_2_1_5.in o=HPGe_model_2_1_5.o tasks 2
+del runt* 
 del HPGe_model_2_2_1.o 
-mcnp6 i=HPGe_model_2_2_1.in o=HPGe_model_2_2_1.o tasks 3
+mcnp6 i=HPGe_model_2_2_1.in o=HPGe_model_2_2_1.o tasks 2
+del runt* 
 del HPGe_model_2_2_18.o 
-mcnp6 i=HPGe_model_2_2_18.in o=HPGe_model_2_2_18.o tasks 3
+mcnp6 i=HPGe_model_2_2_18.in o=HPGe_model_2_2_18.o tasks 2
+del runt* 
+del HPGe_model_2_2_5.o 
+mcnp6 i=HPGe_model_2_2_5.in o=HPGe_model_2_2_5.o tasks 2
+del runt* 
+del HPGe_model_2_3_1.o 
+mcnp6 i=HPGe_model_2_3_1.in o=HPGe_model_2_3_1.o tasks 2
+del runt* 
+del HPGe_model_2_3_18.o 
+mcnp6 i=HPGe_model_2_3_18.in o=HPGe_model_2_3_18.o tasks 2
+del runt* 
+del HPGe_model_2_3_5.o 
+mcnp6 i=HPGe_model_2_3_5.in o=HPGe_model_2_3_5.o tasks 2
+del runt* 
 del HPGe_model_2_4_1.o 
-mcnp6 i=HPGe_model_2_4_1.in o=HPGe_model_2_4_1.o tasks 3
+mcnp6 i=HPGe_model_2_4_1.in o=HPGe_model_2_4_1.o tasks 2
+del runt* 
 del HPGe_model_2_4_18.o 
-mcnp6 i=HPGe_model_2_4_18.in o=HPGe_model_2_4_18.o tasks 3
+mcnp6 i=HPGe_model_2_4_18.in o=HPGe_model_2_4_18.o tasks 2
+del runt* 
+del HPGe_model_2_4_5.o 
+mcnp6 i=HPGe_model_2_4_5.in o=HPGe_model_2_4_5.o tasks 2
+del runt* 
 del HPGe_model_2_5_1.o 
-mcnp6 i=HPGe_model_2_5_1.in o=HPGe_model_2_5_1.o tasks 3
+mcnp6 i=HPGe_model_2_5_1.in o=HPGe_model_2_5_1.o tasks 2
+del runt* 
 del HPGe_model_2_5_18.o 
-mcnp6 i=HPGe_model_2_5_18.in o=HPGe_model_2_5_18.o tasks 3
+mcnp6 i=HPGe_model_2_5_18.in o=HPGe_model_2_5_18.o tasks 2
+del runt* 
+del HPGe_model_2_5_5.o 
+mcnp6 i=HPGe_model_2_5_5.in o=HPGe_model_2_5_5.o tasks 2
+del runt* 
 del *.$$$ 
 del runt* 
