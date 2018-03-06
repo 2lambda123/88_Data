226502c78159a842774bea32ee4557dfd09504a3
jamesbevins <james.e.bevins@gmail.com>
2018-03-06 13:51:01 -0500

    Improved statistics for pt-foil correction at 18 cm

---
+++ Simulated/Activation/33MevTa/HPGeDetFoilSimulations/PtSrcrunMCNP.bat
@@ -0,0 +1,62 @@
+del HPGe_model_10_1_1.o 
+mcnp6 i=HPGe_model_10_1_1.in o=HPGe_model_10_1_1.o tasks 2
+del runt* 
+del HPGe_model_10_1_18.o 
+mcnp6 i=HPGe_model_10_1_18.in o=HPGe_model_10_1_18.o tasks 2
+del runt* 
+del HPGe_model_1_1_1.o 
+mcnp6 i=HPGe_model_1_1_1.in o=HPGe_model_1_1_1.o tasks 2
+del runt* 
+del HPGe_model_1_1_18.o 
+mcnp6 i=HPGe_model_1_1_18.in o=HPGe_model_1_1_18.o tasks 2
+del runt* 
+del HPGe_model_2_1_1.o 
+mcnp6 i=HPGe_model_2_1_1.in o=HPGe_model_2_1_1.o tasks 2
+del runt* 
+del HPGe_model_2_1_18.o 
+mcnp6 i=HPGe_model_2_1_18.in o=HPGe_model_2_1_18.o tasks 2
+del runt* 
+del HPGe_model_3_1_1.o 
+mcnp6 i=HPGe_model_3_1_1.in o=HPGe_model_3_1_1.o tasks 2
+del runt* 
+del HPGe_model_3_1_18.o 
+mcnp6 i=HPGe_model_3_1_18.in o=HPGe_model_3_1_18.o tasks 2
+del runt* 
+del HPGe_model_4_1_1.o 
+mcnp6 i=HPGe_model_4_1_1.in o=HPGe_model_4_1_1.o tasks 2
+del runt* 
+del HPGe_model_4_1_18.o 
+mcnp6 i=HPGe_model_4_1_18.in o=HPGe_model_4_1_18.o tasks 2
+del runt* 
+del HPGe_model_5_1_1.o 
+mcnp6 i=HPGe_model_5_1_1.in o=HPGe_model_5_1_1.o tasks 2
+del runt* 
+del HPGe_model_5_1_18.o 
+mcnp6 i=HPGe_model_5_1_18.in o=HPGe_model_5_1_18.o tasks 2
+del runt* 
+del HPGe_model_6_1_1.o 
+mcnp6 i=HPGe_model_6_1_1.in o=HPGe_model_6_1_1.o tasks 2
+del runt* 
+del HPGe_model_6_1_18.o 
+mcnp6 i=HPGe_model_6_1_18.in o=HPGe_model_6_1_18.o tasks 2
+del runt* 
+del HPGe_model_7_1_1.o 
+mcnp6 i=HPGe_model_7_1_1.in o=HPGe_model_7_1_1.o tasks 2
+del runt* 
+del HPGe_model_7_1_18.o 
+mcnp6 i=HPGe_model_7_1_18.in o=HPGe_model_7_1_18.o tasks 2
+del runt* 
+del HPGe_model_8_1_1.o 
+mcnp6 i=HPGe_model_8_1_1.in o=HPGe_model_8_1_1.o tasks 2
+del runt* 
+del HPGe_model_8_1_18.o 
+mcnp6 i=HPGe_model_8_1_18.in o=HPGe_model_8_1_18.o tasks 2
+del runt* 
+del HPGe_model_9_1_1.o 
+mcnp6 i=HPGe_model_9_1_1.in o=HPGe_model_9_1_1.o tasks 2
+del runt* 
+del HPGe_model_9_1_18.o 
+mcnp6 i=HPGe_model_9_1_18.in o=HPGe_model_9_1_18.o tasks 2
+del runt* 
+del *.$$$ 
+del runt* 
