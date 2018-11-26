7dcd1e217095428fe5efe18cd573dc79664effa3
jamesbevins <james.e.bevins@gmail.com>
2018-11-25 23:07:49 -0500

    Updated 33 MeV Unfolds and simulations with corrected efficiencies

---
+++ Simulated/Activation/33MevTa/HPGeDetFoilSimulations/AuFoil/runMCNP.bat
@@ -0,0 +1,9 @@
+mcnp6 i=Rect-Au_Foil_5_18.in o=Rect-Au_Foil_5_18.o tasks 4
+mcnp6 i=Rect-Au_Foil_147_1.in o=Rect-Au_Foil_147_1.o tasks 4
+mcnp6 i=Rect-Au_Foil_147_18.in o=Rect-Au_Foil_147_18.o tasks 4
+mcnp6 i=Rect-Au_Foil_188_1.in o=Rect-Au_Foil_188_1.o tasks 4
+mcnp6 i=Rect-Au_Foil_188_18.in o=Rect-Au_Foil_188_18.o tasks 4
+mcnp6 i=Rect-Au_Foil_303_1.in o=Rect-Au_Foil_303_1.o tasks 4
+mcnp6 i=Rect-Au_Foil_303_18.in o=Rect-Au_Foil_303_18.o tasks 4
+del *.$$$ 
+del runt* 
---
+++ Simulated/Activation/33MevTa/HPGeDetFoilSimulations/Foil/runMCNP.bat
@@ -0,0 +1,8 @@
+mcnp6 i=HPGe_model_147_1_1.in o=HPGe_model_147_1_1.o tasks 4
+mcnp6 i=HPGe_model_147_1_18.in o=HPGe_model_147_1_18.o tasks 4
+mcnp6 i=HPGe_model_188_1_1.in o=HPGe_model_188_1_1.o tasks 4
+mcnp6 i=HPGe_model_188_1_18.in o=HPGe_model_188_1_18.o tasks 4
+mcnp6 i=HPGe_model_303_1_1.in o=HPGe_model_303_1_1.o tasks 4
+mcnp6 i=HPGe_model_303_1_18.in o=HPGe_model_303_1_18.o tasks 4
+del *.$$$ 
+del runt* 
